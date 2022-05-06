9017c6e8bf31ff3d1b159ec855991539797377b1
Omar Carpentiero <269868@studenti.unimore.it>
2022-05-06 18:13:29 +0200

    definiti gli attributi statici AbilityPanel.STR etc. etc.

--- src/main/java/swingtest2/AbilityPanel.java
+++ src/main/java/swingtest2/AbilityPanel.java
@@ -8,9 +8,22 @@ import javax.swing.*;
  * @author Omar Carpentiero
  */
 public class AbilityPanel extends JLayeredPane {
+    /**
+     * static attributes are used to define which type of cell is needed
+     */
+    static final int STR = 1;
+    static final int DEX = 2;
+    static final int CON = 3;
+    static final int INT = 4;
+    static final int WIS = 5;
+    static final int CHA = 6;
+
+
     JTextField modifier;
     JTextPane value;
+    int intValue;
     JLabel name;
+    int intName;
 
     /**
      * The constructor can only be called with a String parameter, which represents the name
@@ -22,11 +35,46 @@ public class AbilityPanel extends JLayeredPane {
         this.name=new JLabel(name);
     }
 
-    public String getName(){
-        return this.name.getText();
+    /**
+     * @return an integer representation of the modifier of the cell
+     */
+    public int getModifier(){
+        return Integer.parseInt(this.modifier.getText());
     }
 
-    public void setName(String name){
-        this.name.setText(name);
+    /**
+     * This method is private so that only the setValue method can access it
+     * @param modifier is the new modifier
+     */
+    private void setModifier(int modifier){
+        if(modifier>=0){
+            this.modifier.setText("+" + String.valueOf(modifier));
+        }
+        else{
+            this.modifier.setText(String.valueOf(modifier));
+        }
     }
+
+    /**
+     * @return an int representation of the name displayed on the cell
+     */
+    @Override
+    public String getName(){
+        switch(intName) {
+            case STR:
+                return "STRENGTH";
+            case DEX:
+                return "DEXTERITY";
+            case CON:
+                return "CONSTITUTION";
+            case INT:
+                return "INTELLIGENCE";
+            case WIS:
+                return "WISDOM";
+            case CHA:
+                return "CHARISMA";
+        }
+        return "ciao";
+    }
+
 }
