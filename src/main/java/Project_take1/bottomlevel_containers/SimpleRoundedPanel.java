9eb05295af7b47dde4c4efc03e04eb953da6116d
Omar Carpentiero <269868@studenti.unimore.it>
2022-06-25 18:25:44 +0200

    migliorie

--- src/main/java/Project_take1/bottomlevel_containers/SimpleRoundedPanel.java
+++ src/main/java/Project_take1/bottomlevel_containers/SimpleRoundedPanel.java
@@ -1,36 +1,110 @@
 package Project_take1.bottomlevel_containers;
 
 import Project_take1.resources.graphics.RoundedJPanel;
+import org.intellij.lang.annotations.JdkConstants;
 
 import javax.swing.*;
 import java.awt.*;
-
+//this RoundedJPanel only has one big label, for the nomber of the stat, and one small label, to define the name of it
 public class SimpleRoundedPanel extends RoundedJPanel {
-    String name;
-    String number;
-    public SimpleRoundedPanel(String name, String number){
-        this.name = name;
-        this.number= number;
+    JLabel numberLabel;
+    JLabel nameLabel;
+    public SimpleRoundedPanel(String name, String number,int arcWidth,int arcHeight){
+        this.arcWidth=arcWidth;
+        this.arcHeight=arcHeight;
 
-        JLabel numberLabel=new JLabel(number);
-        JLabel valueLabel=new JLabel(name);
+        JLabel numberLabel=new JLabel();
+        JLabel nameLabel =new JLabel();
+        numberLabel.setText(number);
+        nameLabel.setText(name);
+
         JPanel contentPanel=new JPanel(new GridBagLayout());
-
+        contentPanel.setPreferredSize(new Dimension(50,60));
+        contentPanel.setOpaque(false);
         GridBagConstraints c=new GridBagConstraints();
 
+        numberLabel.setFont(new Font("Comic Sans",Font.BOLD,30));
+        numberLabel.setHorizontalAlignment(JLabel.CENTER);
+        c.weighty=10;
+        c.weightx=5;
+        c.gridy=0;
+        c.fill=GridBagConstraints.BOTH;
+        numberLabel.setPreferredSize(new Dimension(50,30));
+        contentPanel.add(numberLabel,c);
+
+        nameLabel.setFont(new Font("Comic Sans",Font.BOLD,10));
+        nameLabel.setHorizontalAlignment(JLabel.CENTER);
         c.weighty=1;
-        c.fill=GridBagConstraints.HORIZONTAL;
+        c.gridy=1;
+        c.weightx=5;
+        contentPanel.add(nameLabel,c);
 
-
+        add(contentPanel);
     }
 
+    //Constructor for panel with only one label
+    public SimpleRoundedPanel(String name,int arcWidth,int arcHeight){
+        this.arcWidth=arcWidth;
+        this.arcHeight=arcHeight;
+
+        JLabel nameLabel =new JLabel();
+        nameLabel.setText(name);
+
+        JPanel contentPanel=new JPanel();
+        contentPanel.setPreferredSize(new Dimension(50,60));
+        contentPanel.setOpaque(false);
+
+        nameLabel.setFont(new Font("Comic Sans",Font.BOLD,30));
+        nameLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        nameLabel.setPreferredSize(new Dimension(50,30));
+        contentPanel.add(nameLabel);
+
+        add(contentPanel);
+    }
+
+    public void setName(String name){
+        if(nameLabel!=null){
+            this.nameLabel.setText(name);
+        }
+        else{
+            throw new IllegalStateException("The SimpleRoundedPanel with associated number"+numberLabel.getText()+
+                    "does not have a name");
+        }
+    }
+
+    public void setNumber(String number){
+        if(nameLabel!=null){
+            this.numberLabel.setText(number);
+        }
+        else{
+            throw new IllegalStateException("The SimpleRoundedPanel with associated name"+nameLabel.getText()+
+                    "does not have a number");
+        }
+    }
+
+    public void setNumber(int number){
+        if(nameLabel!=null){
+            this.numberLabel.setText(Integer.toString(number));
+        }
+        else{
+            throw new IllegalStateException("The SimpleRoundedPanel with associated name"+nameLabel.getText()+
+                    "does not have a number");
+        }
+    }
     @Override
     public void paintComponent(Graphics g){
-        setArcs(30,30);
+        setArcs(arcWidth,arcHeight);
+        super.paintComponent(g);
 
     }
 
     @Override
     public void updateColors() {
+    }
+
+    public void setNameAlignement(int horizontalAlignment,int verticalAlignment){
+        nameLabel.setHorizontalAlignment(horizontalAlignment);
+        nameLabel.setVerticalAlignment(verticalAlignment);
     }
 }
--- src/main/java/Project_take1/toplevel_containers/SecondColumn.java
+++ src/main/java/Project_take1/toplevel_containers/SecondColumn.java
@@ -1,24 +1,38 @@
 package Project_take1.toplevel_containers;
 
-import Project_take1.bottomlevel_containers.HealthPanel;
 import Project_take1.bottomlevel_containers.SimpleRoundedPanel;
+import Project_take1.resources.graphics.RoundedJPanel;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class SecondColumn extends JPanel {
     public SecondColumn(){
+        GridBagConstraints c=new GridBagConstraints();
         setLayout(new BorderLayout());
         setOpaque(false);
-        setPreferredSize(new Dimension(200,800));
+        setPreferredSize(new Dimension(230,800));
         JPanel placeHolder=new JPanel();
         placeHolder.setPreferredSize(new Dimension(placeHolder.getPreferredSize().width,800));
         add(placeHolder,BorderLayout.CENTER);
-        JPanel grid_1 =new JPanel(new GridLayout(1,3,4,4));
-        grid_1.add(new SimpleRoundedPanel("AC","12"));
-        grid_1.add(new SimpleRoundedPanel("INIT","+5"));
-        grid_1.add(new SimpleRoundedPanel("SPEED","9m"));
-        grid_1.setOpaque(false);
+
+        JPanel grid_1=new JPanel(new GridBagLayout());
+        SimpleRoundedPanel className=new SimpleRoundedPanel("Paladin",30,30);
+        SimpleRoundedPanel level=new SimpleRoundedPanel("LVL","13",30,30);
+        c.fill=GridBagConstraints.BOTH;
+        c.weightx=10;
+        grid_1.add(className,c);
+        c.weightx=2;
+        grid_1.add(level,c);
+
+        JPanel grid_2 =new JPanel(new GridLayout(1,3,4,4));
+        grid_2.add(new SimpleRoundedPanel("AC","12",30,30));
+        grid_2.add(new SimpleRoundedPanel("INIT","+5",30,30));
+        grid_2.add(new SimpleRoundedPanel("SPEED","9m",30,30));
+        grid_2.setOpaque(false);
+        grid_2.setPreferredSize(new Dimension(grid_2.getPreferredSize().width,70));
+
         add(grid_1,BorderLayout.NORTH);
+        add(grid_2,BorderLayout.CENTER);
     }
 }
